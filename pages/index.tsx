2baf2568d1b1b90c12f885f2014d7726d0b0d847
Joaquin <joaquin.buxo@gmail.com>
2022-09-19 18:45:42 +0200

    structuring main components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Grid from "../components/Grid/Grid";
+import Card from "../components/Card/Card";
+import Spinner from "../components/Spinner/Spinner";
 
 const Home: NextPage = () => {
-  return <main>testingo</main>;
+  return (
+    <main className="relative h-screen overflow-y-scroll">
+      <Header></Header>
+      <Hero></Hero>
+      <Grid></Grid>
+      <Card></Card>
+      <Spinner></Spinner>
+    </main>
+  );
 };
 
 export default Home;
