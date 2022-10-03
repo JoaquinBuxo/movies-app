b5d9afb6df0d08e5d1b044cb846dcc5c70ac311f
Joaquin <joaquin.buxo@gmail.com>
2022-10-03 22:49:35 +0200

    fixing eslint error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
 
-const Error: NextPage = () => <div>This rout doesn't exist...</div>;
+const Error: NextPage = () => <div>This route does not exist...</div>;
 
 export default Error;
